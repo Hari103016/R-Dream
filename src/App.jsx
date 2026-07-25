4d6a706ee6660691265193f97a0c3fd49d49c6df
Hari103016 <harigopaladdagatla9@gmail.com>
2026-07-25 12:07:02 +0530

    Remove EditCustomer route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import CustomerDetails from "./pages/CustomerDetails";
-import EditCustomer from "./pages/EditCustomer";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/customer/:id" element={<CustomerDetails />} />
-      <Route path="/edit-customer/:id" element={<EditCustomer />} />
     </Routes>
   );
 }
